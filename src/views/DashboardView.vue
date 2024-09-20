a2a97f32926f3a357dc08dd76728b799e6fa9092
Michel-Habib197 <126814378+Michel-Habib197@users.noreply.github.com>
2024-09-20 19:39:34 -0300

    Commit tela 3 sexta
    Adição dos cabeçalhos do json

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -9,5 +9,15 @@
 
 <script setup>
 import ChartComponent from '@/components/ChartComponent.vue';
+import {ref, onMounted} from 'vue'
 
+const headers = [
+ {title:'Product Name', align: 'start', key:'Product Name'},
+ {title: 'Sales', align: 'end', key: 'Sales'},
+ {title:'City', align: 'end', key: 'City'},
+ {title:'State', align: 'end', key: 'State'},
+ {title:'Country', align: 'end', key: 'Country'},
+ {title:'Order Date', align: 'end', key: 'Order Date'},
+ {title:'Category', align: 'end', key: 'Category'}
+]
 </script>
